a035409a93765fc0575253e2dc65ae46714fee9c
anitanancy2811 <anitanancy2811@gmail.com>
2021-12-17 21:28:21 -0700

    changes

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title>Landing Page</title>
+    
+    <style>
+       
+      body {
+        
+          margin: 2rem;
+        }
+
+
+
+      .container {
+         display: flex;
+      }
+
+      .flex1 {
+         width: 35%;
+      }
+
+
+       img {
+         width: 400px;
+         height: 400px;
+       }
+
+
+
+    </style>
+
+</head>
+<body>
+    <?php 
+      echo "<header>\n\t\t";
+      echo "<h1>The Twins \"Samara and Tamara\"</h1>\n";
+      echo "\t</header>\n";
+      echo "\t\t<div class=\"container\">\n";
+      echo "\t\t\t<div class=\"flex-1\">\n";
+      echo "\t\t\t\t<h2>About my kids</h2>\n";
+      echo "\t\t\t\t\t<p>One of the sweetest memorable moments in my life is when my gynecologist recommended getting a   scan to see I am still pregnant or not and then that day turns out to be announcing that I am carrying twins</p>\n";
+      echo "\t\t\t\t\t<p>After surviving cancer when Doctor told me that I have less chance of me getting pregnant and by God’s grace in a year I got pregnant with these two little miracles and I named them,</p>\n";
+      echo "\t\t\t\t\t\t<ul>
+           \t\t\t\t\t<li>Samara</li>
+           \t\t\t\t\t<li>Tamara</li>
+         \t\t\t\t</ul>\n";
+      echo "\t\t\t\t\t<p>The birth of a child is always a gift, but the birth of the Twins is extra special. Over the years they have both brought so much happiness to me making this world a better place. They were created together, and their connection is something mysteriously wonderful.</p>\n";
+
+      echo "\t\t\t\t<h3>Bio Links</h3>\n";
+      echo "\t\t\t\t\t<a href=\"samara\">Samara</a>\n";
+      echo "\t\t\t\t\t<a href=\"tamara\">Tamara</a>\n";
+      echo "\t\t\t</div>\n";
+      echo "\t\t\t<div class=\"flex-2\">\n";
+      echo "\t\t\t\t<img src=\"samara-tamara.jpg\">\n";
+      echo "\t\t\t</div>\n";
+      echo "\t\t</div>\n";
+      echo "</body>\n";
+      echo "</html>\n";
+
+    ?>
+
+   
+         
+         
+
+
+   
+
+     
+       
